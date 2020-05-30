9aa0019f8a4673e6d17f5a86820915bea614a91f
Marvin Bechtold <marvin.bechtold@gmail.com>
2020-05-30 16:13:35 +0200

    getOrGenerateSigningKeys

--- src/main/java/de/marvin/sodium/SignatureUtil.java
+++ src/main/java/de/marvin/sodium/SignatureUtil.java
@@ -9,21 +9,41 @@ import com.goterl.lazycode.lazysodium.utils.KeyPair;
 
 import java.io.*;
 
+import static de.marvin.sodium.KeyIO.readKeyFromFile;
 import static de.marvin.sodium.KeyIO.writeKeyToFile;
 
 public class SignatureUtil {
 
     private static LazySodiumJava lazySodium = new LazySodiumJava(new SodiumJava());
 
+    public static KeyPair getOrGenerateSigningKeys() throws IOException, SodiumException {
+        return getOrGenerateSigningKeys("");
+    }
+
     public static KeyPair generateSigningKeys() throws SodiumException, IOException {
+        return generateSigningKeys("");
+    }
+
+    public static KeyPair getOrGenerateSigningKeys(String pathToKeys) throws IOException, SodiumException {
+        try {
+            Key publicKey = readKeyFromFile(pathToKeys + "public.key");
+            Key secretKey = readKeyFromFile(pathToKeys + "secret.key");
+            return new KeyPair(publicKey, secretKey);
+        } catch (IOException e) {
+            System.out.println("Generate new key pair.");
+        }
+        return generateSigningKeys(pathToKeys);
+    }
+
+    public static KeyPair generateSigningKeys(String pathToKeys) throws SodiumException, IOException {
         KeyPair kp = lazySodium.cryptoSignKeypair();
         Key publicKey = kp.getPublicKey();
         Key secretKey = kp.getSecretKey();
         if (!lazySodium.cryptoSignKeypair(publicKey.getAsBytes(), secretKey.getAsBytes())) {
             throw new SodiumException("Could not generate a signing keypair.");
         }
-        writeKeyToFile(publicKey, "public.key");
-        writeKeyToFile(secretKey, "secret.key");
+        writeKeyToFile(publicKey, pathToKeys + "public.key");
+        writeKeyToFile(secretKey, pathToKeys + "secret.key");
         return kp;
     }
 
